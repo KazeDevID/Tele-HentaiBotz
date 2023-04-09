a727b9f2508fe9302ba04560030cbca431dc0195
Kaze <101078139+KazeDevID@users.noreply.github.com>
2023-04-09 16:35:31 +0800

    Add files via upload

--- plugin/start.py
+++ plugin/start.py
@@ -1,1 +1,5 @@
+from pyrogram import *
+from pyrogram.types import *
 
+def start(client, message):
+    message.reply_text("""**/search <hentai name> to search**\n Powered By @KenalSayaaa""", parse_mode="markdown")
